4b0d3e66ce0096135a41eaa1c0a283b283dee50c
Robert Long <robert@robertlong.me>
2022-09-16 12:31:00 -0700

    Fix options type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import {ViteDevServer} from "vite";
 import history from "connect-history-api-fallback";
 import {Request, Response} from "express-serve-static-core";
+import { Url } from 'url';
 
-export default function redirectAll(options: history.Options) {
+interface Options {
+    readonly disableDotRule?: true | undefined;
+    readonly htmlAcceptHeaders?: ReadonlyArray<string> | undefined;
+    readonly index?: string | undefined;
+    readonly logger?: typeof console.log | undefined;
+    readonly rewrites?: ReadonlyArray<Rewrite> | undefined;
+    readonly verbose?: boolean | undefined;
+}
+
+interface Context {
+    readonly match: RegExpMatchArray;
+    readonly parsedUrl: Url;
+}
+type RewriteTo = (context: Context) => string;
+
+interface Rewrite {
+    readonly from: RegExp;
+    readonly to: string | RegExp | RewriteTo;
+}
+
+
+export default function redirectAll(options?: Options) {
     return {
         name: "rewrite-all",
         configureServer(server:ViteDevServer) {
-            const { rewrites, ...rest } = options;
+            const { rewrites, ...rest } = options || {};
 
             return () => {
                 const handler = history({
